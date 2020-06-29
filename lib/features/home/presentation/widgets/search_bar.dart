2e6b54311085881086578de14b632d6423563cd1
TheNsBhasin <nsbhasincool@gmail.com>
2020-06-29 14:11:11 +0530

    Add suggestion box to search bar

--- lib/features/home/presentation/widgets/search_bar.dart
+++ lib/features/home/presentation/widgets/search_bar.dart
@@ -6,21 +6,58 @@ class SearchBar extends StatefulWidget {
 }
 
 class _SearchBarState extends State<SearchBar> {
+  bool showSuggestions;
+  FocusNode _focus = new FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+
+    showSuggestions = false;
+    _focus.addListener(_onFocusChange);
+  }
+
+  void _onFocusChange() {
+    if (_focus.hasFocus) {
+      setState(() {
+        showSuggestions = true;
+      });
+    } else {
+      setState(() {
+        showSuggestions = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 16.0, left: 16, right: 16),
-        child: TextField(
-          decoration: InputDecoration(
-            hintText: 'Search your city, resources, etc',
-            prefixIcon: Icon(Icons.search),
-            filled: true,
-            fillColor: Color(0x7417060),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+    return Column(
+      children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16.0, left: 16, right: 16),
+            child: TextField(
+              focusNode: _focus,
+              decoration: InputDecoration(
+                hintText: 'Search your city, resources, etc',
+                prefixIcon: Icon(Icons.search),
+                filled: true,
+                fillColor: Color(0x7417060),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+              ),
+            ),
           ),
         ),
-      ),
+        if (this.showSuggestions != null && this.showSuggestions)
+          _buildSuggestionBox()
+      ],
+    );
+  }
+
+  Widget _buildSuggestionBox() {
+    return SizedBox(
+      height: 200,
     );
   }
 }
