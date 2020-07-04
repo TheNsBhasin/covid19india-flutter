f70415f75055bab0195824d7a0329bcc8bc24087
TheNsBhasin <nsbhasincool@gmail.com>
2020-07-04 21:36:45 +0530

    Fix footer theme

--- lib/features/home/presentation/widgets/footer.dart
+++ lib/features/home/presentation/widgets/footer.dart
@@ -18,40 +18,50 @@ class Footer extends StatelessWidget {
                 color: Colors.grey, fontSize: 14, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 32),
-          RichText(
-              text: TextSpan(
-                  text: 'COVID19',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey[200]),
-                  children: [
-                TextSpan(
-                    text: 'IN',
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(2.0),
+              color: Theme.of(context).primaryColor,
+            ),
+            child: RichText(
+                text: TextSpan(
+                    text: 'COVID19',
                     style: TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
-                        color: Colors.orangeAccent)),
-                TextSpan(
-                    text: 'D',
-                    style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white)),
-                TextSpan(
-                    text: 'IA',
-                    style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.greenAccent)),
-              ])),
+                        color: Colors.grey[200]),
+                    children: [
+                  TextSpan(
+                      text: 'IN',
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.orangeAccent)),
+                  TextSpan(
+                      text: 'D',
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                  TextSpan(
+                      text: 'IA',
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.greenAccent)),
+                ])),
+          ),
           SizedBox(height: 32),
           FlatButton.icon(
             onPressed: _launchURL,
             padding: const EdgeInsets.all(8.0),
             materialTapTargetSize: MaterialTapTargetSize.padded,
             color: Colors.black,
-            icon: FaIcon(FontAwesomeIcons.github),
+            icon: FaIcon(
+              FontAwesomeIcons.github,
+              color: Colors.white,
+            ),
             label: Text("Open Sourced on GitHub",
                 style: TextStyle(
                     color: Colors.white,
